f8c1ab8f62e588cc54feff441933f00dbe64b7ae
avi-codesmith <avi.codesmith@gmail.com>
2026-03-12 22:08:41 +0530

    Mutation Callbacks (like: onSuccess) Query Invalidation

--- src/components/Events/EventForm.jsx
+++ src/components/Events/EventForm.jsx
@@ -1,9 +1,16 @@
-import { useState } from 'react';
-
-import ImagePicker from '../ImagePicker.jsx';
+import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import ImagePicker from "../ImagePicker.jsx";
+import { fetchSelectableImages } from "../../util/http.js";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
 
 export default function EventForm({ inputData, onSubmit, children }) {
   const [selectedImage, setSelectedImage] = useState(inputData?.image);
+
+  const { data, isPending, isError } = useQuery({
+    queryKey: ["event-images"],
+    queryFn: ({ signal }) => fetchSelectableImages(signal),
+  });
 
   function handleSelectImage(image) {
     setSelectedImage(image);
@@ -26,24 +33,35 @@ export default function EventForm({ inputData, onSubmit, children }) {
           type="text"
           id="title"
           name="title"
-          defaultValue={inputData?.title ?? ''}
+          defaultValue={inputData?.title ?? ""}
         />
       </p>
-
-      <div className="control">
-        <ImagePicker
-          images={[]}
-          onSelect={handleSelectImage}
-          selectedImage={selectedImage}
+      {isPending && (
+        <p style={{ color: "orange" }}>
+          Please wait loading some selectable images
+        </p>
+      )}
+      {isError && (
+        <ErrorBlock
+          title="Failed to load selectable images"
+          message={"Please try again"}
         />
-      </div>
-
+      )}
+      {data && (
+        <div className="control">
+          <ImagePicker
+            images={data || []}
+            onSelect={handleSelectImage}
+            selectedImage={selectedImage}
+          />
+        </div>
+      )}
       <p className="control">
         <label htmlFor="description">Description</label>
         <textarea
           id="description"
           name="description"
-          defaultValue={inputData?.description ?? ''}
+          defaultValue={inputData?.description ?? ""}
         />
       </p>
 
@@ -54,7 +72,7 @@ export default function EventForm({ inputData, onSubmit, children }) {
             type="date"
             id="date"
             name="date"
-            defaultValue={inputData?.date ?? ''}
+            defaultValue={inputData?.date ?? ""}
           />
         </p>
 
@@ -64,7 +82,7 @@ export default function EventForm({ inputData, onSubmit, children }) {
             type="time"
             id="time"
             name="time"
-            defaultValue={inputData?.time ?? ''}
+            defaultValue={inputData?.time ?? ""}
           />
         </p>
       </div>
@@ -75,7 +93,7 @@ export default function EventForm({ inputData, onSubmit, children }) {
           type="text"
           id="location"
           name="location"
-          defaultValue={inputData?.location ?? ''}
+          defaultValue={inputData?.location ?? ""}
         />
       </p>
 
--- src/components/Events/NewEvent.jsx
+++ src/components/Events/NewEvent.jsx
@@ -1,25 +1,52 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from "react-router-dom";
 
-import Modal from '../UI/Modal.jsx';
-import EventForm from './EventForm.jsx';
+import Modal from "../UI/Modal.jsx";
+import EventForm from "./EventForm.jsx";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
+import { useMutation } from "@tanstack/react-query";
+import { createNewEvent, queryClient } from "../../util/http.js";
 
 export default function NewEvent() {
   const navigate = useNavigate();
 
-  function handleSubmit(formData) {}
+  const { mutate, isPending, isError, error } = useMutation({
+    // mutationKey: ["formEvent"], We can though we don't wanna save data on server or backend hence we dont need any key here, just we're positing data.
+    mutationFn: createNewEvent,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["event"] });
+      navigate("/events");
+    },
+  });
+
+  function handleSubmit(formData) {
+    mutate({ event: formData });
+    console.log(formData);
+  }
 
   return (
-    <Modal onClose={() => navigate('../')}>
+    <Modal onClose={() => navigate("../")}>
       <EventForm onSubmit={handleSubmit}>
-        <>
-          <Link to="../" className="button-text">
-            Cancel
-          </Link>
-          <button type="submit" className="button">
-            Create
-          </button>
-        </>
+        {isPending ? (
+          "Submitting..."
+        ) : (
+          <>
+            <Link to="../" className="button-text">
+              Cancel
+            </Link>
+            <button type="submit" className="button">
+              Create
+            </button>
+          </>
+        )}
       </EventForm>
+      {isError && (
+        <ErrorBlock
+          title="Failed to Send"
+          message={
+            error.info?.message || "Please check your inputs or try again!"
+          }
+        />
+      )}
     </Modal>
   );
 }
